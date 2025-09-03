1e5ab6bff895d3a32fb7be7a854f43116d2b7522
Pascal Knoth <pascal@malach.it>
2025-09-03 09:18:12 +0200

    prefer node integration testing

--- packages/core/test/integration/locationHandler.test.ts
+++ packages/core/test/integration/locationHandler.test.ts
@@ -1,124 +1,47 @@
-import { render, screen, waitFor } from "@testing-library/react";
-// biome-ignore lint/correctness/noUnusedImports: required by tsx files
-import React, { useState } from "react";
-import { assert, describe, expect, it, vi } from "vitest";
-import { Core, type IssuerMetadata } from "../../src";
-import { OauthError } from "../../src/errors";
+import { assert, describe, expect, it } from "vitest";
 import { locationHandlerFactory } from "../../src/handlers";
 
-const core = new Core({});
-
-type PushedAuthorizationRequestMetadata = {
-	issuer_state: string;
-	issuer_metadata: IssuerMetadata;
-};
-
-const LocationHandler = () => {
-	const [success, setSuccess] = useState(false);
-	const [error, setError] = useState<OauthError | null>(null);
-	const [protocol, setProtocol] = useState<string | null>(null);
-	const [nextStep, setNextStep] = useState<string | undefined>(undefined);
-	const [data, setData] = useState<
-		PushedAuthorizationRequestMetadata | undefined
-	>(undefined);
-
-	(async () => {
-		return core
-			.location(window.location)
-			.then(({ protocol, nextStep, data }) => {
-				setSuccess(true);
-				setProtocol(protocol);
-				setNextStep(nextStep);
-				setData(data);
-			})
-			.catch((error) => {
-				if (error instanceof OauthError) {
-					return setError(error);
-				}
-				console.log(error);
-			});
-	})();
-
-	return (
-		<div>
-			{error && <div data-testid="error">{error.error_description}</div>}
-			{success && (
-				<div data-testid="success">
-					{protocol && <p data-testid="protocol">{protocol}</p>}
-					{nextStep && <p data-testid="nextStep">{nextStep}</p>}
-					{data && <p data-testid="issuerState">{data.issuer_state}</p>}
-				</div>
-			)}
-		</div>
-	);
-};
-
-describe("location handler - integration", () => {
-	it("returns", () => {
-		render(<LocationHandler />);
-
-		return waitFor(() => {
-			const success = screen.getByTestId("success") as HTMLAnchorElement;
-			assert(success);
-			expect(success.innerHTML).to.eq("");
-		});
-	});
-
-	it("returns an error with an invalid credential offer", () => {
-		vi.stubGlobal("location", {
-			search: "?credential_offer=invalid",
-		});
-		render(<LocationHandler />);
-
-		return waitFor(() => {
-			const error = screen.getByTestId("error") as HTMLAnchorElement;
-			assert(error);
-			expect(error.innerHTML).to.eq("credential offer could not be parsed");
-		});
-	});
-
-	it("renders with a valid authorization code grants (issuer_state)", () => {
-		const credential_issuer = "https://demo-issuer.wwwallet.org/";
-		const credential_configuration_ids = ["credential_configuration_ids"];
-		const issuer_state = "issuer_state";
-		const grants = { authorization_code: { issuer_state } };
-		const credential_offer = {
-			credential_issuer,
-			credential_configuration_ids,
-			grants,
-		};
-		vi.stubGlobal("location", {
-			search: `?credential_offer=${JSON.stringify(credential_offer)}`,
-		});
-		render(<LocationHandler />);
-
-		return waitFor(() => {
-			screen.getByTestId("success") as HTMLAnchorElement;
-
-			const protocol = screen.getByTestId("protocol") as HTMLAnchorElement;
-			expect(protocol.innerHTML).to.eq("oid4vci");
-
-			const nextStep = screen.getByTestId("nextStep") as HTMLAnchorElement;
-			expect(nextStep.innerHTML).to.eq("pushed_authorization_request");
-
-			const issuerState = screen.getByTestId(
-				"issuerState",
-			) as HTMLAnchorElement;
-			expect(issuerState.innerHTML).to.eq(issuer_state);
-		});
-	});
-});
-
-describe("location handler - handler", () => {
+describe("location handler", () => {
 	const locationHandler = locationHandlerFactory({});
+
+	it("returns", async () => {
+		const location = {
+			search: "",
+		};
+
+		// @ts-ignore
+		const response = await locationHandler(location);
+
+		expect(response.protocol).to.eq(null);
+	});
+
+	it("returns an error with an invalid credential offer", async () => {
+		const credential_offer = "invalid";
+		const location = {
+			search: `?credential_offer=${credential_offer}`,
+		};
+
+		try {
+			// @ts-ignore
+			await locationHandler(location);
+
+			assert(false);
+		} catch (error) {
+			expect(error.error).to.eq("invalid_location");
+			expect(error.error_description).to.eq(
+				"credential offer could not be parsed",
+			);
+		}
+	});
 
 	it("returns an error with an invalid credential offer (empty)", async () => {
 		const credential_offer = {};
-		vi.stubGlobal("location", {
+		const location = {
 			search: `?credential_offer=${JSON.stringify(credential_offer)}`,
-		});
+		};
 
 		try {
+			// @ts-ignore
 			await locationHandler(location);
 
 			assert(false);
@@ -135,11 +58,12 @@ describe("location handler - handler", () => {
 		const credential_offer = {
 			credential_issuer,
 		};
-		vi.stubGlobal("location", {
+		const location = {
 			search: `?credential_offer=${JSON.stringify(credential_offer)}`,
-		});
+		};
 
 		try {
+			// @ts-ignore
 			await locationHandler(location);
 
 			assert(false);
@@ -158,11 +82,12 @@ describe("location handler - handler", () => {
 			credential_issuer,
 			credential_configuration_ids,
 		};
-		vi.stubGlobal("location", {
+		const location = {
 			search: `?credential_offer=${JSON.stringify(credential_offer)}`,
-		});
+		};
 
 		try {
+			// @ts-ignore
 			await locationHandler(location);
 
 			assert(false);
@@ -181,11 +106,12 @@ describe("location handler - handler", () => {
 			credential_issuer,
 			credential_configuration_ids,
 		};
-		vi.stubGlobal("location", {
+		const location = {
 			search: `?credential_offer=${JSON.stringify(credential_offer)}`,
-		});
+		};
 
 		try {
+			// @ts-ignore
 			await locationHandler(location);
 
 			assert(false);
@@ -204,11 +130,12 @@ describe("location handler - handler", () => {
 			credential_issuer,
 			credential_configuration_ids,
 		};
-		vi.stubGlobal("location", {
+		const location = {
 			search: `?credential_offer=${JSON.stringify(credential_offer)}`,
-		});
+		};
 
 		try {
+			// @ts-ignore
 			await locationHandler(location);
 
 			assert(false);
@@ -229,11 +156,12 @@ describe("location handler - handler", () => {
 			credential_configuration_ids,
 			grants,
 		};
-		vi.stubGlobal("location", {
+		const location = {
 			search: `?credential_offer=${JSON.stringify(credential_offer)}`,
-		});
+		};
 
 		try {
+			// @ts-ignore
 			await locationHandler(location);
 
 			assert(false);
@@ -254,11 +182,12 @@ describe("location handler - handler", () => {
 			credential_configuration_ids,
 			grants,
 		};
-		vi.stubGlobal("location", {
+		const location = {
 			search: `?credential_offer=${JSON.stringify(credential_offer)}`,
-		});
+		};
 
 		try {
+			// @ts-ignore
 			await locationHandler(location);
 
 			assert(false);
@@ -279,11 +208,12 @@ describe("location handler - handler", () => {
 			credential_configuration_ids,
 			grants,
 		};
-		vi.stubGlobal("location", {
+		const location = {
 			search: `?credential_offer=${JSON.stringify(credential_offer)}`,
-		});
+		};
 
 		try {
+			// @ts-ignore
 			await locationHandler(location);
 
 			assert(false);
@@ -304,10 +234,11 @@ describe("location handler - handler", () => {
 			credential_configuration_ids,
 			grants,
 		};
-		vi.stubGlobal("location", {
+		const location = {
 			search: `?credential_offer=${JSON.stringify(credential_offer)}`,
-		});
+		};
 
+		// @ts-ignore
 		const response = await locationHandler(location);
 
 		expect(response.protocol).to.eq("oid4vci");
@@ -328,10 +259,11 @@ describe("location handler - handler", () => {
 			credential_configuration_ids,
 			grants,
 		};
-		vi.stubGlobal("location", {
+		const location = {
 			search: `?credential_offer=${JSON.stringify(credential_offer)}`,
-		});
+		};
 
+		// @ts-ignore
 		const response = await locationHandler(location);
 
 		expect(response.protocol).to.eq("oid4vci");
