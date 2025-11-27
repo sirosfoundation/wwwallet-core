90e60abab434f2b065f3e937057f6d86ef614409
Pascal Knoth <pascal@malach.it>
2025-11-27 18:30:00 +0100

    [client-core] deferred credential handler implementation

---
+++ packages/client-core/src/handlers/deferredCredential.handler.ts
@@ -0,0 +1,134 @@
+import Ajv from "ajv";
+import type { Config } from "../config";
+import { OauthError } from "../errors";
+import type { DeferredCredential } from "../resources";
+import {
+	type FetchCredentialsConfig,
+	type FetchIssuerMetadataConfig,
+	fetchCredentials,
+	fetchIssuerMetadata,
+	type GenerateDpopConfig,
+	generateDpop,
+} from "../statements";
+import { deferredCredentialHandlerConfigSchema } from "./schemas/deferredCredentialHandlerConfig.schema";
+
+const ajv = new Ajv();
+
+export type DeferredCredentialHandlerParams = {
+	deferred_credential: DeferredCredential;
+};
+
+export type DeferredCredentialHandlerConfig = FetchCredentialsConfig &
+	FetchIssuerMetadataConfig &
+	GenerateDpopConfig;
+
+type DeferredCredentialHandlerProtocol = "oid4vci";
+
+type DeferredCredentialHandlerNextStep = "deferred_credentials_success";
+
+type DeferredCredentialResponse = {
+	protocol: DeferredCredentialHandlerProtocol;
+	nextStep: DeferredCredentialHandlerNextStep;
+	data: {
+		deferred_credential: DeferredCredential;
+		credentials: Array<{ credential: string }> | undefined;
+	};
+};
+
+const protocol = "oid4vci";
+const currentStep = "deferred_credentials";
+const nextStep = "deferred_credentials_success";
+
+export function deferredCredentialHandlerFactory(
+	config: DeferredCredentialHandlerConfig,
+) {
+	return async function deferredCredentialHandler({
+		deferred_credential,
+	}: DeferredCredentialHandlerParams): Promise<DeferredCredentialResponse> {
+		try {
+			const { client_state } = deferred_credential;
+
+			const { issuer_metadata } = await fetchIssuerMetadata(
+				{
+					issuer: client_state.issuer,
+					client_state,
+				},
+				config,
+			);
+
+			// TODO access token management
+			const access_token = "access_token";
+
+			const { dpop: credentialsDpop } = await generateDpop(
+				{
+					client_state,
+					access_token,
+					htu: issuer_metadata.credential_endpoint,
+					htm: "POST",
+				},
+				config,
+			);
+
+			const { credentials } = await fetchCredentials(
+				{
+					issuer_metadata,
+					access_token,
+					dpop: credentialsDpop,
+					deferred_credential,
+				},
+				config,
+			);
+
+			return {
+				protocol,
+				nextStep,
+				data: {
+					deferred_credential,
+					credentials,
+				},
+			};
+		} catch (error) {
+			if (error instanceof OauthError) {
+				const data = deferredCredentialErrorData({
+					protocol,
+					currentStep,
+					nextStep,
+					deferred_credential,
+				});
+				throw error.toResponse(data);
+			}
+
+			throw error;
+		}
+	};
+}
+
+export function validateDeferredCredentialHandlerConfig(config: Config) {
+	const validate = ajv.compile(deferredCredentialHandlerConfigSchema);
+	if (!validate(config)) {
+		const errorText = ajv.errorsText(validate.errors);
+
+		throw new Error(
+			`Could not validate handler template configuration - ${errorText}`,
+		);
+	}
+}
+
+function deferredCredentialErrorData({
+	protocol,
+	currentStep,
+	nextStep,
+	deferred_credential,
+}: {
+	protocol: string;
+	currentStep: string;
+	nextStep: string;
+	deferred_credential: DeferredCredential;
+}) {
+	return {
+		protocol,
+		currentStep,
+		nextStep,
+		deferred_credential,
+	};
+}
---
+++ packages/client-core/test/integration/deferredCredentialHandler.test.ts
@@ -0,0 +1,180 @@
+import { assert, describe, expect, it } from "vitest";
+import { OauthError } from "../../src";
+import { deferredCredentialHandlerFactory } from "../../src/handlers";
+import {
+	clientStateStoreMock,
+	fetchIssuerMetadataMock,
+	httpClientPostMock,
+} from "../support/client";
+
+describe("credentialHandler", () => {
+	const issuer = "http://issuer.url";
+	const state = "state";
+
+	it("rejects without deferred credential endpoint in issuer metadata", async () => {
+		const config = {
+			httpClient: {
+				post: httpClientPostMock(),
+				get: fetchIssuerMetadataMock({
+					issuer,
+				}),
+			},
+			clientStateStore: clientStateStoreMock({
+				state,
+			}),
+			dpop_ttl_seconds: 10,
+		};
+
+		const credentialHandler = deferredCredentialHandlerFactory(config);
+		const deferred_credential = {
+			client_state: await config.clientStateStore.fromState(state),
+			transaction_id: "transaction_id",
+		};
+
+		try {
+			await credentialHandler({
+				deferred_credential,
+			});
+
+			assert(false);
+		} catch (error) {
+			if (!(error instanceof OauthError)) {
+				throw error;
+			}
+
+			expect(error.error).to.eq("invalid_parameters");
+			expect(error.error_description).to.eq(
+				"deferred credential endpoint is missing in issuer metadata",
+			);
+			expect(error.data).toMatchObject({
+				currentStep: "deferred_credentials",
+				deferred_credential: {
+					client_state: {
+						code_verifier: "code_verifier",
+						dpopKeyPair: {
+							privateKey: {
+								alg: "ES256",
+							},
+							publicKey: {},
+						},
+						issuer: "http://issuer.url",
+						issuer_state: "issuer_state",
+						state: "state",
+					},
+					transaction_id: "transaction_id",
+				},
+				nextStep: "deferred_credentials_success",
+				protocol: "oid4vci",
+			});
+		}
+	});
+
+	it("resolves when deferred credential request rejects", async () => {
+		const config = {
+			httpClient: {
+				// @ts-ignore
+				post: async (..._params) => {
+					throw new Error("rejected");
+				},
+				get: fetchIssuerMetadataMock({
+					issuer,
+					deferred_credential_endpoint: new URL(
+						"/deferred-credential",
+						issuer,
+					).toString(),
+				}),
+			},
+			clientStateStore: clientStateStoreMock({
+				state,
+			}),
+			dpop_ttl_seconds: 10,
+		};
+
+		const deferred_credential = {
+			client_state: await config.clientStateStore.fromState(state),
+			transaction_id: "transaction_id",
+		};
+
+		const credentialHandler = deferredCredentialHandlerFactory(config);
+
+		try {
+			await credentialHandler({
+				deferred_credential,
+			});
+
+			assert(false);
+		} catch (error) {
+			if (!(error instanceof OauthError)) {
+				throw error;
+			}
+
+			expect(error.error).to.eq("invalid_request");
+			expect(error.error_description).to.eq(
+				"could not fetch deferred credential",
+			);
+			expect(error.data).toMatchObject({
+				currentStep: "deferred_credentials",
+				deferred_credential: {
+					client_state: {
+						code_verifier: "code_verifier",
+						dpopKeyPair: {
+							privateKey: {
+								alg: "ES256",
+							},
+							publicKey: {},
+						},
+						issuer: "http://issuer.url",
+						issuer_state: "issuer_state",
+						state: "state",
+					},
+					transaction_id: "transaction_id",
+				},
+				error: new Error("rejected"),
+				nextStep: "deferred_credentials_success",
+				protocol: "oid4vci",
+			});
+		}
+	});
+
+	it("resolves when deferred credential request returns credentials", async () => {
+		const config = {
+			httpClient: {
+				// @ts-ignore
+				post: async <T>(..._params) => {
+					return { data: { credentials: [{ credential: "credential" }] } } as T;
+				},
+				get: fetchIssuerMetadataMock({
+					issuer,
+					deferred_credential_endpoint: new URL(
+						"/deferred-credential",
+						issuer,
+					).toString(),
+				}),
+			},
+			clientStateStore: clientStateStoreMock({
+				state,
+			}),
+			dpop_ttl_seconds: 10,
+		};
+
+		const deferred_credential = {
+			client_state: await config.clientStateStore.fromState(state),
+			transaction_id: "transaction_id",
+		};
+
+		const credentialHandler = deferredCredentialHandlerFactory(config);
+
+		const response = await credentialHandler({
+			deferred_credential,
+		});
+
+		expect(response).to.deep.eq({
+			data: {
+				credentials: [{ credential: "credential" }],
+				deferred_credential,
+			},
+			nextStep: "deferred_credentials_success",
+			protocol: "oid4vci",
+		});
+	});
+});
